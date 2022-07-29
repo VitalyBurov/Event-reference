e85c5e0cbbd40d69ad2573d97991abfe9cefa32c
VitalyBurov <77235095+VitalyBurov@users.noreply.github.com>
2022-07-30 01:17:59 +0300

    Delete User directory

--- User/src/main/java/by/burov/user/core/enums/UserRole.java
+++
@@ -1,7 +0,0 @@
-package by.burov.user.core.enums;
-
-public enum UserRole {
-    USER, ADMIN
-
-
-}
--- User/src/main/java/by/burov/user/core/enums/UserStatus.java
+++
@@ -1,5 +0,0 @@
-package by.burov.user.core.enums;
-
-public enum UserStatus {
-    WAITING_ACTIVATION, ACTIVATED, DEACTIVATED
-}
