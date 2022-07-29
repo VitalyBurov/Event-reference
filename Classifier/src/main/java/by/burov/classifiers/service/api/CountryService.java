fc4bc0541402539bcf5e6db1a4faf81e687827d9
VitalyBurov <77235095+VitalyBurov@users.noreply.github.com>
2022-07-30 01:16:27 +0300

    Delete Classifier directory
    Delete classifier folder

--- Classifier/src/main/java/by/burov/classifiers/service/api/CountryService.java
+++
@@ -1,8 +0,0 @@
-package by.burov.classifiers.service.api;
-
-import by.burov.classifiers.core.dto.CreateCountryDto;
-import by.burov.classifiers.core.dto.ReadCountryDto;
-
-public interface CountryService extends ClassifierService<CreateCountryDto, ReadCountryDto>{
-
-}
